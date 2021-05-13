e1da7a2702d317c1fb94bf0794ae27c05dc83c55
Hasanabasori Samang <hasanbasori.sa@gmail.com>
2021-05-13 13:49:22 +0700

    create axios config file

---
+++ src/configs/axios.config.js
@@ -0,0 +1,29 @@
+import Axios from 'axios'
+
+import localStorageService from '../services/localStorageService'
+
+Axios.defaults.baseURL = 'http://localhost:8000'
+
+//intercept request ส่ง header ไปให้ back end
+Axios.interceptors.request.use(
+  (config) => {
+    if (localStorageService.getToken())
+      config.headers.Authorization = `Bearer ${localStorageService.getToken()}`
+    return config
+  },
+  (err) => Promise.reject(err) //คล้ายๆ next function
+)
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (err) => {
+    if (err.response.status === 401) {
+      localStorageService.clearToken()
+      window.location.assign('/') // window = globalObject, location = redirect path
+      return
+    }
+    return Promise.reject(err)
+  }
+)
+
+export default Axios
